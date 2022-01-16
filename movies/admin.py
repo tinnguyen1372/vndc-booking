4500a34731a54e68228d78c2a6de2f6ef5b34711
tinnguyen02 <thanhtin001@e.ntu.edu.sg>
2022-01-17 05:39:45 +0800

    second deployment

--- movies/admin.py
+++ movies/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Movie,  Seat
-# Payment, PaymentIntent,
+from .models import Movie,  Seat, Payment, PaymentIntent
+
 # Register your models here.
 admin.site.register(Movie)
 admin.site.register(Seat)
-# admin.site.register(Payment)
-# admin.site.register(PaymentIntent)
+admin.site.register(Payment)
+admin.site.register(PaymentIntent)
