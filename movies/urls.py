f2017d4faa495dd74502e7cad3b42beea292daf2
tinnguyen02 <thanhtin001@e.ntu.edu.sg>
2022-01-16 19:01:20 +0800

    push

--- movies/urls.py
+++ movies/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-
+from .views import index
+app_name = 'movies'
 urlpatterns = [
-    
+    path('', index, name="home")
 ]
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Movie
 # Create your views here.
+
+
+def index(request):
+    movies = Movie.objects.all()
+    return render(request, 'index.html', {
+        "movies": movies
+    })
