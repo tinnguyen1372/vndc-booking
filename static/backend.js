f8e95e2ef69a6b177e668ef60a856f5a6e905381
tinnguyen02 <thanhtin001@e.ntu.edu.sg>
2022-01-17 19:32:26 +0800

    deploying

--- static/backend.js
+++ static/backend.js
@@ -14,8 +14,8 @@ async function contactAPI(url, body) {
 }
 
 function refreshSeat() {
-    const movie_title = movieSelect.options[movieSelect.selectedIndex].id
-
+    // const movie_title = movieSelect.options[movieSelect.selectedIndex].id
+    const movie_title = 'VNDC ($1)'
     contactAPI("/occupied/", { movie_title }).then(data => {
         const occupied_seat = data['occupied_seats']
         const movie_title = data["movie"].replace(/\s/g, '')
