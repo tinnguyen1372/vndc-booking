b2474b79dc4850aa06364249f4e78b55f777cbf3
tinnguyen02 <thanhtin001@e.ntu.edu.sg>
2022-01-17 19:19:01 +0800

    deploy

--- movieWebsite/settings.py
+++ movieWebsite/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATIC_ROOT = BASE_DIR/'static_root'
