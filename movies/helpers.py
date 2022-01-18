cb1032c1a5cd4e6ce3733105c1e211647af62a39
tinnguyen02 <thanhtin001@e.ntu.edu.sg>
2022-01-18 16:32:45 +0800

    deploy

--- movies/helpers.py
+++ movies/helpers.py
@@ -6,20 +6,27 @@ from django.template.loader import render_to_string
 from django.core.mail import send_mail
 
 
-def verify_webook(request):
-    secret = bytes(settings.PAYSTACK_SECRET, "utf-8")
-    digester = hmac.new(secret, request.body, hashlib.sha512)
-    calculated_signature = digester.hexdigest()
-    if calculated_signature == request.META["HTTP_X_PAYSTACK_SIGNATURE"]:
-        return True
-    return False
+# def verify_webook(request):
+#     secret = bytes(settings.PAYSTACK_SECRET, "utf-8")
+#     digester = hmac.new(secret, request.body, hashlib.sha512)
+#     calculated_signature = digester.hexdigest()
+#     if calculated_signature == request.META["HTTP_X_PAYSTACK_SIGNATURE"]:
+#         return True
+#     return False
 
 
 def email_customer(first_name, seat_no, movie_title, email):
+    d = {2: 'A18', 3: 'A17', 4: 'A16', 5: 'A15', 6: 'A14', 7: 'A13', 8: 'A12', 9: 'A11', 10: 'A10', 11: 'A9', 12: 'A8', 13: 'A7', 14: 'A6', 15: 'A5', 16: 'A4', 17: 'A3', 18: 'A2', 19: 'A1',
+         24: 'B18', 25: 'B17', 26: 'B16', 27: 'B15', 28: 'B14', 29: 'B13', 30: 'B12', 31: 'B11', 32: 'B10', 33: 'B9', 34: 'B8', 35: 'B7', 36: 'B6', 37: 'B5', 38: 'B4', 39: 'B3', 40: 'B2', 41: 'B1',
+         46: 'C18', 47: 'C17', 48: 'C16', 49: 'C15', 50: 'C14', 51: 'C13', 52: 'C12', 53: 'C11', 54: 'C10', 55: 'C9', 56: 'C8', 57: 'C7', 58: 'C6', 59: 'C5', 60: 'C4', 61: 'C3', 62: 'C2', 63: 'C1',
+         68: 'D18', 69: 'D17', 70: 'D16', 71: 'D15', 72: 'D14', 73: 'D13', 74: 'D12', 75: 'D11', 76: 'D10', 77: 'D9', 78: 'D8', 79: 'D7', 80: 'D6', 81: 'D5', 82: 'D4', 83: 'D3', 84: 'D2', 85: 'D1',
+         90: 'E18', 91: 'E17', 92: 'E16', 93: 'E15', 94: 'E14', 95: 'E13', 96: 'E12', 97: 'E11', 98: 'E10', 99: 'E9', 100: 'E8', 101: 'E7', 102: 'E6', 103: 'E5', 104: 'E4', 105: 'E3', 106: 'E2', 107: 'E1',
+         108: 'F22', 109: 'F21', 110: 'F20', 111: 'F19', 112: 'F18', 113: 'F17', 114: 'F16', 115: 'F15', 116: 'F14', 117: 'F13', 118: 'F12', 119: 'F11', 120: 'F10', 121: 'F9', 123: 'F8', 124: 'F7', 125: 'F6', 127: 'F5',
+         128: 'F4', 129: 'F3', 130: 'F2', 131: 'F1'}
     render_msg = render_to_string("email_template.html", {
         "first_name": first_name,
         "movie_title": movie_title,
-        "seat_no": seat_no
+        "seat_no": d[seat_no]
     })
 
     send_mail(
